90462e7512d38209d962e5155c26dceb29aac3cf
e1berd <114188050+eckeriaue@users.noreply.github.com>
2022-09-25 17:30:45 +0300

    add new layout

--- index.php
+++ index.php
@@ -135,7 +135,7 @@
 
 
 
-    <section class="mt-[64px] bg-[color:var(--blue)] rounded-t-[56px]  pt-80">
+    <section class="mt-[64px] bg-[color:var(--blue)] rounded-t-[56px]  pt-80 pb-[calc(48px+56px)]">
       <div class="container flex flex-wrap gap-y-[24px] gap-x-[26px] justify-center">
 
         <?php
@@ -150,7 +150,7 @@
         for ($i = 0; $i < count($about_blocks); $i++) :
         ?>
 
-          <div class="border-4 border-white p-8 rounded-[24px] flex flex-col justify-between">
+          <div class="border-4 group hover:scale-105 transition-transform border-white p-8 rounded-[24px] flex flex-col justify-between">
             <div> <img src="<?= $about_blocks[$i]['img'] ?>" alt="<?= $about_blocks[$i]['img'] ?>"> </div>
             <p class="max-w-[272px] text-white text-[16px] leading-[100%] ">
               <?= $about_blocks[$i]['text'] ?>
@@ -163,10 +163,88 @@
     </section>
 
 
-    <section>
-      <div class="container"></div>
+    <section style="background-image: url('./assets/img/about/about-logo-bg.png'); background-position: 350px;" class="-mt-[56px]  bg-no-repeat rounded-t-[56px] bg-white pb-[240px]">
+      <div class="container flex justify-end  pt-48">
+
+        <div class="max-w-[516px]">
+          <h3 class=" mb-[37px]
+              heading relative
+              before:absolute before:content-[''] before:-bottom-[8px] before:left-0 before:w-[160px]
+              before:h-[3px] before:rounded-lg before:bg-[color:var(--blue-2)]
+            ">
+            О нас
+          </h3>
+
+          <div class="text-[16px] text-black leading-[130%] flex flex-col gap-y-10">
+
+            <p>
+              Компания «Комплексное промышленное снабжение» является структурным
+              подразделением, специализирующимся на поставках
+              запчастей (грузовых вагонов и локомотивов) по всей территории России, а также
+              Азии — Казахстана, Узбекистана, Туркменистана, Кыргызстана,
+              Таджикистана, Кавказа – Грузия, Азербайджан и Ближнего Востока – Иран.
+            </p>
+            <p>
+              Мы сотрудничаем с крупными отечественными и зарубежными предприятиями,
+              специализирующимися на изготовлении запчастей для вагонов и локомотивов.
+              Крупные поставки товара позволяют нам осуществлять продажу по
+              низким ценам и всегда иметь необходимые детали на складах в большом количестве.
+            </p>
+            <p>
+              Компания «Комплексное промышленное снабжение» предлагает комплексные решения для
+              клиентов производственных и ремонтных отраслях экономики,
+              предоставляя товар необходимого качества и
+              высочайший уровень обслуживания. В результате, Вы получите
+              плановое, оперативное и эффективное снабжение
+              предприятия в нужном ассортименте и по
+              выгодным ценам.
+            </p>
+
+
+
+          </div>
+
+        </div>
+
+      </div>
     </section>
 
+
+
+    <section>
+      <div class="container">
+        <div>
+          <h3 class="heading text-center mb-24"> Каталог </h3>
+        </div>
+
+
+        <div class="grid grid-cols-3 gap-x-8 justify-center">
+          <?php
+          $catalog_layout = [
+            ['img' => './assets/img/main/catalog/catalog-main-page-1.png', 'title' => 'Запчасти на грузовые вагоны'],
+            ['img' => './assets/img/main/catalog/catalog-main-page-2.png', 'title' => 'Запчасти на локомотивы '],
+            ['img' => './assets/img/main/catalog/catalog-main-page-3.png', 'title' => 'Услуги (логистика, хранение)'],
+          ];
+
+          for ($i = 0; $i < count($catalog_layout); $i++) : ?>
+
+            <div class="border-2 border-[color:var(--blue-2)] flex flex-col justify-between rounded-[24px] p-8">
+
+              <div class="w-[222px] mx-auto">
+                <img class="w-full" src="<?= $catalog_layout[$i]['img'] ?>" alt="<?= $catalog_layout[$i]['img'] ?>">
+              </div>
+
+              <p class="text-center font-semibold text-[18px] leading-[100%] text-[color:var(--dark)]"> <?= $catalog_layout[$i]['title'] ?> </p>
+
+              <button class="w-full text-center border border-[color:var(--blue-2)] py-2 rounded-full text-[14px]"> Подробнее </button>
+
+            </div>
+
+          <?php endfor; ?>
+        </div>
+
+      </div>
+    </section>
 
 
   </main>
